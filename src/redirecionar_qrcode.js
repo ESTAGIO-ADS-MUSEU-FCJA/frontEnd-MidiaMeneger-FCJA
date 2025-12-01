c980d3e993c0c2e2ab6478ce78df890f9ab62e28
RAMOS <pedrohramos083@gmail.com>
2025-12-01 19:49:03 -0300

    link para app qrcode proprio

--- src/redirecionar_qrcode.js
+++ src/redirecionar_qrcode.js
@@ -1,6 +1,5 @@
 // 1. Defina a URL para onde você deseja redirecionar
-const urlDeDestino = "https://qrcodecreator.com/pt?gclid=Cj0KCQiAoZDJBhC0ARIsAERP-F9tq1a3Juvu_-qjuqMD2YhroJHo5JkgLD7yGvFEcBVVGLQE91sd0wIaAgYsEALw_wcB&AdId=722224834698&keyword=gerador%20qr&matchtype=e&device=c&devicemodel=&loc=9101452&network=g&place=&campaignid=21933808051&gad_source=1&gad_campaignid=21933808051&gbraid=0AAAAAqLtWn0zRe_2Fk_D3SkCBszOvaP5S&gclid=Cj0KCQiAoZDJBhC0ARIsAERP-F9tq1a3Juvu_-qjuqMD2YhroJHo5JkgLD7yGvFEcBVVGLQE91sd0wIaAgYsEALw_wcB"; 
-
+const urlDeDestino = "https://seu-qrcode.vercel.app/"
 // 2. Encontra o botão usando seu ID
 // O código é executado somente quando o HTML está carregado
 const botaoRedirecionar = document.getElementById('redirect-qrcode-button');
